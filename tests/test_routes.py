1c2a7266d57c9a447dec3d7c025163ce3ec224cf
SHAYAN ROY <shayanroy74@gmail.com>
2026-06-19 01:52:05 +0530

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -37,7 +37,7 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
-        
+
         talisman.force_https = False
 
     @classmethod
@@ -216,3 +216,8 @@ class TestAccountService(TestCase):
                 response.headers["Referrer-Policy"],
                 "strict-origin-when-cross-origin"
             )
+        
+        self.assertEqual(
+                response.headers["Access-Control-Allow-Origin"],
+                    "*"
+        )
